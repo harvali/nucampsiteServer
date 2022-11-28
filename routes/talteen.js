8ed453c9ebaa1956260b95813e8a257a28776147
harvali <97155122+harvali@users.noreply.github.com>
2022-11-27 14:20:11 -1000

    assigment_for_review

--- routes/talteen.js
+++ routes/talteen.js
@@ -1,30 +1,32 @@
-const express = require("express");
-const favoriteRouter = express.Router();
-const authenticate = require("../authenticate");
-const Favorite = require("../models/favorite");
-const cors = require("./cors");
+const express = require('express');
+const Favorite = require('../models/favorite');
+const authenticate = require('../authenticate');
+const cors = require('./cors');
+const user = require('../models/user');
+const { register } = require('../models/favorite');
 
-favoriteRouter
-    .route("/")
+const favoriteRouter = express.Router();
+
+favoriteRouter.route('/')
     .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
-    .get(cors.cors, authenticate.verifyUser, (req, res, next) => {
+    .get(cors.cors, (req, res, next) => {
         Favorite.find({ user: req.user._id })
-            .populate("user")
-            .populate("campsites")
-            .then((favorite) => {
+            .populate('user')
+            .populate('campsites')
+            .then(favorite => {
                 res.statusCode = 200;
-                res.setHeader("Content-Type", "application/json");
+                res.setHeader('Content-Type', 'application/json');
                 res.json(favorite);
             })
-            .catch((err) => next(err));
+            .catch(err => next(err));
     })
     .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
         Favorite.findOne({ user: req.user._id })
             .then((favorite) => {
                 if (favorite) {
-                    req.body.forEach((fav) => {
+                    register.body.forEach((fav) => {
                         if (!favorite.campsites.includes(fav._id)) {
-                            favorite.campsites.push(fav._id);
+                            favorite.campsite.push(fav._id);
                         }
                     });
                     favorite
@@ -59,7 +61,7 @@ favoriteRouter
     })
     .put(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
         res.statusCode = 403;
-        res.end("PUT operation not supported on /favorites");
+        res.end('PUT operation not supported on /campsites');
     })
     .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
         Favorite.findOneAndDelete({ user: req.user._id })
@@ -70,27 +72,24 @@ favoriteRouter
                     res.json(favorite);
                 } else {
                     res.setHeader("Content-Type", "text/plain");
-                    res.end("You do not have any favorites to delete.");
+                    res.end("You do not have any favorites to delete")
                 }
             })
-            .catch((err) => next(err));
+            .catch(err => next(err));
     });
 
-favoriteRouter
-    .route("/:campsiteId")
+favoriteRouter.route('/:campsiteId')
     .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
-    .get(cors.cors, authenticate.verifyUser, (req, res) => {
+    .get(cors.cors, authenticate.verifyUser, (req, res, next) => {
         res.statusCode = 403;
-        res.end(
-            `GET operation not supported on /favorites/${req.params.campsiteId}`
-        );
+        res.end(`GET operation not supported on /favorites/${req.params.campsiteId}`);
     })
-    .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
+    .post(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
         Favorite.findOne({ user: req.user._id })
             .then((favorite) => {
                 if (favorite) {
                     if (!favorite.campsites.includes(req.params.campsiteId)) {
-                        favorite.campsites.push(req.params.campsiteId);
+                        favorite.campsite.push(req.params.campsiteId);
                         favorite
                             .save()
                             .then((favorite) => {
@@ -102,12 +101,11 @@ favoriteRouter
                     } else {
                         res.statusCode = 200;
                         res.setHeader("Content-Type", "text/plain");
-                        res.end("That campsite is already a favorite!");
+                        res.end("Already exist in favorites!")
                     }
                 } else {
                     Favorite.create({
-                        user: req.user._id,
-                        campsites: [req.params.campsiteId],
+                        user: req.user._id, campsites: [req.params.campsiteId],
                     })
                         .then((favorite) => {
                             res.statusCode = 200;
@@ -119,11 +117,9 @@ favoriteRouter
             })
             .catch((err) => next(err));
     })
-    .put(cors.corsWithOptions, authenticate.verifyUser, (req, res) => {
+    .put(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
         res.statusCode = 403;
-        res.end(
-            `PUT operation not supported on /favorites/${req.params.campsiteId}`
-        );
+        res.end(`PUT operation not supported on /favorites/${req.params.campsiteId}`);
     })
     .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
         Favorite.findOne({ user: req.user._id })
@@ -140,14 +136,14 @@ favoriteRouter
                             res.setHeader("Content-Type", "application/json");
                             res.json(favorite);
                         })
-                        .catch((err) => next(err));
+                        .catch(err => next(err));
                 } else {
                     res.statusCode = 200;
                     res.setHeader("Content-Type", "text/plain");
-                    res.end("You do not have any favorites to delete.");
+                    res.end("No favorites to delete");
                 }
             })
-            .catch((err) => next(err));
+            .catch(err => next(err));
     });
 
 module.exports = favoriteRouter;
